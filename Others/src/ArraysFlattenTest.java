15a0b0aab0313408c9f91b0addbc24fed1e14477
ohahohah <ohahohah.dev@gmail.com>
2018-05-31 09:58:51 +0900

    Fix Junit path

--- Others/src/ArraysFlattenTest.java
+++ Others/src/ArraysFlattenTest.java
@@ -1,15 +1,15 @@
-import static org.junit.Assert.*;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArraysFlattenTest {
 
-  @org.junit.Before
+  @Before
   public void setUp() throws Exception {
 
   }
 
-  @org.junit.After
+  @After
   public void tearDown() throws Exception {
   }
 
